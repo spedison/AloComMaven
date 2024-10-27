7be4484b7cb98ecf29ea9c4de6d691d2130baa92
spedison <spedison@gmail.com>
2024-10-27 15:10:36 -0300

    Gerando executavel com graalvm

--- src/main/java/br/com/spedison/Main.java
+++ src/main/java/br/com/spedison/Main.java
@@ -1,5 +1,7 @@
 package br.com.spedison;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,7 +10,10 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        int[] arrInicial = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arrFinal = ArrayUtils.addAll(arrInicial, 30, 50, 100);
+
+        for (int i : arrFinal) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
